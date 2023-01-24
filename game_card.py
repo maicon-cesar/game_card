fef505e4f4bf7effdb55bd8becbb02c9e516746f
Maicon Cesar Canales de Oliveira <maicon.cesar@gmail.com>
2023-01-24 07:24:30 -0300

    Added class button

--- game_card.py
+++ game_card.py
@@ -1,5 +1,4 @@
 import sys, pygame
-from pygame import font
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, image_file, location, scale):
@@ -11,25 +10,41 @@ class Sprite(pygame.sprite.Sprite):
         self.rect.left, self.rect.top = location
 
 class Button():
-    pass
+    text = None
+    x = 0
+    y = 0
 
+    def __init__(self, text, x, y):
+        self.x = x
+        self.y = y
+        color = (255,255,255)
+        smallfont = pygame.font.SysFont('Corbel', 35) 
+        self.text = smallfont.render(text, True, color) 
+
+
+    def draw_button(self, screen, mouse):
+        color_light = (170,170,170)
+        color_dark = (100,100,100)
+
+        if self.x <= mouse[0] <= self.x+140 and self.y <= mouse[1] <= self.y+40:
+            pygame.draw.rect(screen, color_light, [self.x, self.y, 140, 40], 3, 3)
+        else: 
+            pygame.draw.rect(screen, color_dark, [self.x, self.y, 140, 40], 3, 3)
+
+        screen.blit(self.text, (self.x+50, self.y+9))
+
+    def click(self, mouse):
+        if self.x <= mouse[0] <= self.x+140 and self.y <= mouse[1] <= self.y+40:
+            return True
+        else:
+            return False
+    
 pygame.init()
 
 DEFAULT_WINDOW_SIZE = 727, 441
 screen = pygame.display.set_mode(DEFAULT_WINDOW_SIZE)
 pygame.display.set_caption('Exercise Python Maicon')
 COLOR_BLACK = 0,0,0
-
-
-# Inicia btn exit
-color = (255,255,255) 
-color_light = (170,170,170)
-color_dark = (100,100,100) 
-width = 0#screen.get_width() 
-height = 0#screen.get_height() 
-smallfont = pygame.font.SysFont('Corbel', 35) 
-text = smallfont.render('Exit', True, color) 
-  
 
 background = Sprite('images/background.png', [0,50], (0,0))
 
@@ -43,14 +58,18 @@ fonte=pygame.font.get_default_font()
 fontesys=pygame.font.SysFont(fonte, 30)
 txttela = fontesys.render(txt, 1, (255,255,255))
 
+button_red = Button('RED', 0, 400)
+button_black = Button('BLACK', 200, 400)
+button_exit = Button('Exit', 400, 400)
+
 while True:
     mouse = pygame.mouse.get_pos() 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
-        if event.type == pygame.MOUSEBUTTONDOWN: 
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button_exit.click(mouse):
                 sys.exit()
 
     screen.fill(COLOR_BLACK)
@@ -58,11 +77,8 @@ while True:
     screen.blit(card_image.image, card_image.rect)
     screen.blit(txttela,(5,10))
 
-    if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-        pygame.draw.rect(screen, color_light, [width/2, height/2, 140, 40], 3, 3)
-    else: 
-        pygame.draw.rect(screen, color_dark, [width/2, height/2, 140, 40], 3, 3)
-
-    screen.blit(text, (width/2+50,height/2+9))
+    button_red.draw_button(screen, mouse)
+    button_black.draw_button(screen, mouse)
+    button_exit.draw_button(screen, mouse)
 
     pygame.display.flip()
